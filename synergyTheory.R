db528558b7cb9e126e6b4c43ffb6c7e7c91015f7
Edward Huang <edwardghg@gmail.com>
2019-12-18 16:32:06 -0800

    Commenting and style edits.

--- synergyTheory.R
+++ synergyTheory.R
@@ -9,27 +9,33 @@
 # Contact:      Rainer K. Sachs 
 # Website:      https://github.com/rainersachs/LSSR_HG_2019
 # Mod history:  17 Dec 2019
-# Details:      See data_info.R for further licensing, attribution,
+# Details:      See dataInfo.R for further licensing, attribution,
 #               references, and abbreviation information.
 
-source("dataInfo.R") # Load in the data. Remark: dose is in units of cGy; 
-# LET usually in keV/micron; prevalence Prev always < 1
-# (i.e. not in %, which would mean prevalence < 100 but is strongly deprecated).
+source("dataInfo.R") # Load in the data. 
+# Remark: dose is in units of cGy;LET usually in keV/micron; 
+# prevalence Prev always < 1 (i.e. not in %, which would mean 
+# prevalence < 100 but is strongly deprecated).
 
 library(deSolve) # Solving differential equations.
 library(dplyr)
+
 #========================= MISC. OBJECTS & VARIABLES ==========================#
 # In next line phi controls how fast NTE build up from zero; not really needed 
-# during calibration since phi * Dose >> 1 at every observed Dose !=0. phi is
+# during calibration since phi * Dose >> 1 at every observed Dose != 0. phi is
 # needed for later synergy calculations. 
 # d_0 = 1 / phi = 5 x 10-4 cGy = 5 x 10^-6 Gy.
+
+# If a paper uses dose in Gy some care is required in the 
+# following lines to rescale from cGy.
 
 phi <- 2000 # Even larger phi should give the same final results, 
             # but might cause extra problems with R. 
 Y_0 <- 0.046404
-Y_0 <- 0.025 # values for robustness check
+# Y_0 <- 0.025 # values for robustness check
 # Y_0 <- 0.041
 message("Current value of Y_0: ", Y_0)
+
 
 #================================ DER MODELS ==================================#
 
@@ -42,75 +48,86 @@ message("Current value of Y_0: ", Y_0)
 # summary(beta_decay_lm, correlation = TRUE)
 
 #=============== HZE/NTE MODEL =================#
-# HZE = high charge and energy; 
-# select(filter(HZE_data, Beam == "O")
-# NTE = non-targeted effects are included in addition to TE)
-# HZE_data <- ion_data[c(13:47,49:52), ] # Includes 1-ion data iff Z > 3 
+# HZE = high charge and energy.
+# NTE = non-targeted effects are included in addition to TE.
+
+# Includes 1-ion data iff Z > 3.
 HZE_data <- select(filter(ion_data, Z > 3),1:length(ion_data[2,]))
+
 # Uses 3 adjustable parameters. 
-HZE_nte_model <- nls( # Calibrating parameters in a model that modifies the hazard function NTE models in 17Cuc. 
+HZE_nte_model <- nls( # Calibrating parameters in a model that modifies 
+                      # the hazard function NTE models in 17Cuc. 
   Prev ~ Y_0 + (1 - exp ( - (aa1 * LET * dose * exp( - aa2 * LET) 
                                + (1 - exp( - phi * dose)) * kk1))), 
-  data = HZE_data, 
+  data    = HZE_data, 
   weights = NWeight,
-  start = list(aa1 = .00009, aa2 = .001, kk1 = .06)) # Use extra argument trace = TRUE if you want to watch convergence. 
+  start   = list( # Use extra argument trace = TRUE 
+                  # if you want to watch convergence.
+                 aa1 = .00009, 
+                 aa2 = .001, 
+                 kk1 = .06))  
 
-summary(HZE_nte_model, correlation = TRUE) # Parameter values & accuracy.
-# If a paper uses dose in Gy some care is required in the preceding and following lines to rescale from cGy.
+summary(HZE_nte_model, correlation = TRUE) # Parameter values and accuracy.
 vcov(HZE_nte_model) # Variance-covariance matrix.
-HZE_nte_model_coef <- coef(HZE_nte_model) # Calibrated central values of the 3 parameters.
+HZE_nte_model_coef <- coef(HZE_nte_model) # Calibrated central values of the 
+                                          # three parameters.
 
-# The DER, = 0 at dose 0.
-calibrated_nte_hazard_func <- function(dose, LET, coef) { # Calibrated hazard function. 
+# Calibrated hazard function. I.e. the DER, such that the effect is 0 at dose 0.
+calibrated_nte_hazard_func <- function(dose, LET, coef) {
  return(coef[1] * LET * dose * exp( - coef[2] * LET) 
         + (1 - exp( - phi * dose)) * coef[3])
 } 
 
-calibrated_HZE_nte_der <- function(dose, LET, coef = HZE_nte_model_coef) { # Calibrated HZE NTE DER.
+# Calibrated HZE NTE DER.
+calibrated_HZE_nte_der <- function(dose, LET, coef = HZE_nte_model_coef) { 
   return(1 - exp( - calibrated_nte_hazard_func(dose, LET, coef)))
 }
 
 #================ HZE/TE MODEL =================#
-# (TE = targeted effects only). This chunk runs and gives good results. 
+# TE = targeted effects only. 
 
 HZE_te_model <- nls( # Calibrating parameters in a TE only model.
   Prev ~ Y_0 + (1 - exp ( - (aate1 * LET * dose * exp( - aate2 * LET)))),
-  data = HZE_data,
+  data    = HZE_data,
   weights = NWeight,
-  start = list(aate1 = .00009, aate2 = .01))
+  start   = list(aate1 = .00009, aate2 = .01))
 
 summary(HZE_te_model, correlation = TRUE) # Parameter values & accuracy.
 vcov(HZE_te_model) # Variance-covariance matrix.
-HZE_te_model_coef <- coef(HZE_te_model) # Calibrated central values of the 2 parameters. 
+HZE_te_model_coef <- coef(HZE_te_model) # Calibrated central values of the 
+                                        # two parameters. 
 
-# The DER, = 0 at dose 0.
-calibrated_te_hazard_func <- function(dose, LET, coef) { # Calibrated hazard function.
+# Calibrated hazard function. I.e. the DER, such that the effect is 0 at dose 0.
+calibrated_te_hazard_func <- function(dose, LET, coef) { 
   return(coef[1] * LET * dose * exp( - coef[2] * LET))
 }
 
+# Calibrated HZE TE one-ion DER.
 calibrated_HZE_te_der <- function(dose, LET, coef = HZE_te_model_coef) {
-  return(1 - exp( - calibrated_te_hazard_func(dose, LET, coef))) # Calibrated HZE TE one-ion DER.
+  return(1 - exp( - calibrated_te_hazard_func(dose, LET, coef))) 
 }
 
 #==== LIGHT ION, LOW Z (<= 3), LOW LET MODEL ===#
-low_LET_data = ion_data[c(1:12,48), ] # Swift protons and alpha particles. RKS to RKS: need to add extra proton point
+low_LET_data = ion_data[c(1:12,48), ] # Swift protons and alpha particles. 
 low_LET_model <- nls(
-  Prev ~ Y_0 + 1 - exp( - alpha_low * dose), # alpha is used throughout radiobiology for dose coefficients.
-  data = low_LET_data,
+  # alpha is used throughout radiobiology for dose coefficients.
+  Prev ~ Y_0 + 1 - exp( - alpha_low * dose), 
+  data    = low_LET_data,
   weights = NWeight,
-  start = list(alpha_low = .005))
+  start   = list(alpha_low = .005))
 
 summary(low_LET_model, correlation = TRUE)
-low_LET_model_coef <- coef(low_LET_model) # Calibrated central values of the parameter.
+low_LET_model_coef <- coef(low_LET_model) # Calibrated central values of 
+                                          # the parameter.
 
-# Calibrated Low LET model. Use L = 0, but maybe later will use small L > 0.
+# Calibrated Low LET model. Use LET = 0, but maybe later will use small LET > 0.
 calibrated_low_LET_der <- function(dose, LET, alph_low = low_LET_model_coef[1]) {  
   return(1 - exp( - alph_low * dose))
 }  
 
 # Slope dE/dd of the low LET, low Z model.
 low_LET_slope <- function(dose, LET) { 
-  low_LET_model_coef * exp( - low_LET_model_coef * dose)  
+  return(low_LET_model_coef * exp( - low_LET_model_coef * dose))
 }
 
 
@@ -120,10 +137,9 @@ info_crit_table <- cbind(AIC(HZE_te_model, HZE_nte_model),
 print(info_crit_table)
 
 ##=================== Cross validation ====================##
-## ====== ======##
-# Seperate Data into 8 blocks, i.e. test/training sets:
+# Seperate data into 8 blocks, i.e. test/training sets.
 data_len <- 1:length(HZE_data)
-O_350 <- select(filter(HZE_data, LET == 20), data_len) #RKS added 3 oxygen points 5/17/2019)
+O_350 <- select(filter(HZE_data, LET == 20), data_len) 
 Ne_670 <- select(filter(HZE_data, LET == 25), data_len) # Beam data not labeled
 Si_260 <- select(filter(HZE_data, LET == 70), data_len)
 Ti_1000 <- select(filter(HZE_data, LET == 100), data_len)
@@ -131,11 +147,11 @@ Fe_600 <- select(filter(HZE_data, LET == 193), data_len)
 Fe_350 <- select(filter(HZE_data, LET == 253), data_len)
 Nb_600 <- select(filter(HZE_data, LET == 464), data_len)
 La_593 <- select(filter(HZE_data, LET == 953), data_len)
-set_list <- list(O_350, Ne_670, Si_260, Ti_1000,
-                 Fe_600, Fe_350, Nb_600, La_593)
+
+set_list <- list(O_350, Ne_670, Si_260, Ti_1000, Fe_600, Fe_350, Nb_600, La_593)
 actual_prev <- HZE_data$Prev
 
-# # Cross Validation for NTE Model:
+# Cross Validation for NTE Model:
 theoretical <- vector()
 for (i in 1:length(set_list)) {
   test <- set_list[[i]]
@@ -144,14 +160,16 @@ for (i in 1:length(set_list)) {
   for (j in 2:length(excluded_list)) {
     train <- rbind(train, excluded_list[[j]])
   }
-  HZE_nte_model <- nls(Prev ~ Y_0 + (1 - exp ( - (aa1 * LET * dose * exp( - aa2 * LET) + (1 - exp( - phi * dose)) * kk1))),
-                       data = train,
-                       weights = NWeight,
-                       start = list(aa1 = .00009, aa2 = .001, kk1 = .06))
+  HZE_nte_model <- nls(
+    Prev ~ Y_0 + (1 - exp ( - (aa1 * LET * dose * exp( - aa2 * LET) 
+           + (1 - exp( - phi * dose)) * kk1))),
+    data    = train,
+    weights = NWeight,
+    start   = list(aa1 = .00009, aa2 = .001, kk1 = .06))
   predic <- predict(HZE_nte_model, test)
   theoretical <- c(theoretical, predic)
 }
-errors <- (theoretical - actual_prev)^2
+errors <- (theoretical - actual_prev) ^ 2
 NTE_cv <- weighted.mean(errors, HZE_data$NWeight)
 
 #======= Cross Validation for TE Model  ========#
@@ -163,10 +181,11 @@ for (i in 1:8) {
   for (j in 2:length(excluded_list)) {
     train <- rbind(train, excluded_list[[j]])
   }
-  HZE_te_model <- nls(Prev ~ Y_0 + (1 - exp ( - (aate1 * LET * dose * exp( - aate2 * LET)))),
-                      data = train,
-                      weights = NWeight,
-                      start = list(aate1 = .00009, aate2 = .01))
+  HZE_te_model <- nls(
+    Prev ~ Y_0 + (1 - exp ( - (aate1 * LET * dose * exp( - aate2 * LET)))),
+    data    = train,
+    weights = NWeight,
+    start   = list(aate1 = .00009, aate2 = .01))
   predic <- predict(HZE_te_model, test)
   theoretical <- c(theoretical, predic)
 }
@@ -175,7 +194,8 @@ TE_cv <- weighted.mean(errors, HZE_data$NWeight)
 
 CV_table <- cbind(NTE_cv, TE_cv)
 CV_table
-# Dataframe subsetting ---------------------------------------------------------
+
+# Dataframe subsetting 
 
 for (i in 1:length(HZE_data)) { # Columns by index
   print(HZE_data[, i])
@@ -197,12 +217,12 @@ for (i in rownames(HZE_data)) { # Rows by name
 # https://www.r-bloggers.com/5-ways-to-subset-a-data-frame-in-r/
 
 
-## == Rprojects/Auxiliary_files gives toy calculation of completion == ##
-## ======= also see, e.g., R_projects/Old_projects/Chang_2019 ======== ##
-## === But there it is inextricably tangled up with CRAN examples ==== ##
+# Rprojects/Auxiliary_files gives toy calculation of completion. Also see, 
+# e.g., R_projects/Old_projects/Chang_2019 But there it is inextricably tangled 
+# up with CRAN examples.
 
 
-## ===== BASELINE NO-SYNERGY/ANTAGONISM MIXTURE DER FUNCTIONS ======== ##
+#=========== BASELINE NO-SYNERGY/ANTAGONISM MIXTURE DER FUNCTIONS =============#
 
 #======== SIMPLE EFFECT ADDITIVITY (SEA) =======#
 
@@ -211,7 +231,7 @@ for (i in rownames(HZE_data)) { # Rows by name
 #' @param dose Numeric vector corresponding to the sum dose in cGy.
 #' @param LET Numeric vector of all LET values, must be length n.
 #' @param ratios Numeric vector of all dose ratios, must be length n.
-#' @param lowLET Boolean of whether a low LET DER should be included in the mixture DER.
+#' @param lowLET Boolean for whether a low LET DER is included in the mixture DER.
 #' @param n Number of DERs, optional argument used to check parameter validity.
 #' 
 #' @details Corresponding elements of ratios, LET should be associated with the
